b48c1f250e668dc68e5968d82d83a6a144cf7606
Andrei_Dzenicenko <denisenko.andrei.da@gmail.com>
2024-02-12 18:40:10 +0100

    dev: add required fields: age, hobbies

--- src/main.js
+++ src/main.js
@@ -1,100 +1,103 @@
-require('dotenv').config()
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.server = void 0;
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
-const DB = require('./../fakeDB.json');
-
-const PORT = process.env.PORT || 3001;
-
+const PORT = Number(process.env.PORT) || 3000;
+let users = [];
 class Server {
-    port: number
-    constructor(port: number) {
-            this.port = port
+    constructor(port) {
+        this.port = port;
     }
-
     start() {
-        http.createServer((req: any, res: any) => {
+        http.createServer((req, res) => {
             const { method, url } = req;
-
             if (url === '/api/users' && method === 'GET') {
                 // GET all users
-                console.log('/api/users')
                 res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(DB.users));
-            } else if (url.match(/\/api\/users\/\w+/) && method === 'GET') {
+                res.end(JSON.stringify(users));
+            }
+            else if (url.match(/\/api\/users\/\w+/) && method === 'GET') {
                 // GET user by userId
                 const userId = url.split('/')[3];
-                const user = DB.users.find((u: any) => u.id === userId);
+                const user = users.find((u) => u.id === userId);
                 if (!user) {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'User not found' }));
-                } else {
+                }
+                else {
                     res.writeHead(200, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify(user));
                 }
-            } else if (url === '/api/users' && method === 'POST') {
+            }
+            else if (url === '/api/users' && method === 'POST') {
                 // POST create user
                 let body = '';
-                req.on('data', (chunk: any) => {
+                req.on('data', (chunk) => {
                     body += chunk.toString();
                 });
                 req.on('end', () => {
-                    const { name, age, hobbies = [] } = JSON.parse(body);
-                    if (!name && !age && !hobbies.length) {
+                    const { name, email } = JSON.parse(body);
+                    if (!name || !email) {
                         res.writeHead(400, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ error: 'Name, age, hobbies are required' }));
-                    } else {
-                        const newUser = { id: uuidv4(), name, age, hobbies };
-                        DB.users.push(newUser);
+                        res.end(JSON.stringify({ error: 'Name and email are required' }));
+                    }
+                    else {
+                        const newUser = { id: uuidv4(), name, email };
+                        users.push(newUser);
                         res.writeHead(201, { 'Content-Type': 'application/json' });
                         res.end(JSON.stringify(newUser));
                     }
                 });
-            } else if (url.match(/\/api\/users\/\w+/) && method === 'PUT') {
+            }
+            else if (url.match(/\/api\/users\/\w+/) && method === 'PUT') {
                 // PUT update user by userId
                 const userId = url.split('/')[3];
-                const userIndex = DB.users.findIndex((u: any) => u.id === userId);
+                const userIndex = users.findIndex((u) => u.id === userId);
                 if (userIndex === -1) {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'User not found' }));
-                } else {
+                }
+                else {
                     let body = '';
-                    req.on('data', (chunk: any) => {
+                    req.on('data', (chunk) => {
                         body += chunk.toString();
                     });
                     req.on('end', () => {
-                        const { name, age, hobbies = [] } = JSON.parse(body);
-                        if (!name && !age && !hobbies.length) {
+                        const { name, email } = JSON.parse(body);
+                        if (!name || !email) {
                             res.writeHead(400, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ error: 'Name, age, hobbies are required' }));
-                        } else {
-                            DB.users[userIndex] = { ...DB.users[userIndex], name, age, hobbies };
+                            res.end(JSON.stringify({ error: 'Name and email are required' }));
+                        }
+                        else {
+                            users[userIndex] = Object.assign(Object.assign({}, users[userIndex]), { name, email });
                             res.writeHead(200, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify(DB.users[userIndex]));
+                            res.end(JSON.stringify(users[userIndex]));
                         }
                     });
                 }
-            } else if (url.match(/\/api\/users\/\w+/) && method === 'DELETE') {
+            }
+            else if (url.match(/\/api\/users\/\w+/) && method === 'DELETE') {
                 // DELETE user by userId
                 const userId = url.split('/')[3];
-                const userIndex = DB.users.findIndex((u: any) => u.id === userId);
+                const userIndex = users.findIndex((u) => u.id === userId);
                 if (userIndex === -1) {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'User not found' }));
-                } else {
-                    DB.users.splice(userIndex, 1);
+                }
+                else {
+                    users.splice(userIndex, 1);
                     res.writeHead(204);
                     res.end();
                 }
-            } else {
+            }
+            else {
                 // Invalid endpoint
                 res.writeHead(404, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify({ error: 'Endpoint not found' }));
             }
-        }).listen(this.port, () => {
-            console.log(`Server is running on port ${this.port}`);
         });
+        console.log(`Server is running on port ${PORT}`);
     }
-
 }
-
-export const server = new Server(PORT as number)
+exports.server = new Server(PORT);
